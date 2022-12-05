5e5b64a6ae2452c02c18dbed93fc6e08240e888d
진현주 <97022695+realzu@users.noreply.github.com>
2022-12-05 22:17:53 +0900

    update: add jest, react-query

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,21 +1,56 @@
+// React-Query
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import "./App.css";
 import { Posts } from "./react-query/Posts";
 
-const queryClient = new QueryClient();
+// jest
+import logo from './logo.svg';
 
 function App() {
-  return (
-    // provide React Query client to App
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <h1>Blog &apos;em Ipsum</h1>
-        <Posts />
+
+  function Query() {
+    const queryClient = new QueryClient();
+
+    return (
+      // provide React Query client to App
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <h1>Blog &apos;em Ipsum</h1>
+          <Posts />
+        </div>
+        <ReactQueryDevtools /> {/* 개발자도구는 빌드 시 더나은 패키지 관리 위해 하위패키지에서 가져옴 */}
+      </QueryClientProvider>
+    );
+  }
+
+  function Jest() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-      <ReactQueryDevtools /> {/* 개발자도구는 빌드 시 더나은 패키지 관리 위해 하위패키지에서 가져옴 */}
-    </QueryClientProvider>
-  );
+    );
+  }
+
+  return (
+    <>
+      { Query() }
+      { Jest() }
+    </>
+  )
 }
 
 export default App;
