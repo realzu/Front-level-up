8b94259319f7f286d70f2b1a494d3e8b882e37a3
진현주 <97022695+realzu@users.noreply.github.com>
2022-12-11 10:57:38 +0900

    React-query) update: separate React-query file

---
+++ react.js/src/App2.js
@@ -0,0 +1,24 @@
+// React-Query
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "./App.css";
+import { Posts } from "./react-query/Posts";
+
+function App() {
+	const queryClient = new QueryClient();
+
+	return (
+		// provide React Query client to App
+		<QueryClientProvider client={queryClient}>
+			<div>
+				<h1>Blog &apos;em Ipsum</h1>
+				<Posts />
+			</div>
+			<ReactQueryDevtools />{" "}
+			{/* 개발자도구는 빌드 시 더나은 패키지 관리 위해 하위패키지에서 가져옴 */}
+		</QueryClientProvider>
+	);
+}
+
+export default App;
