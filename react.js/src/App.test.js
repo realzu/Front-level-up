243ca8615eb762f89e6068f3cafd19fe88b7d8fe
진현주 <97022695+realzu@users.noreply.github.com>
2022-12-13 22:56:54 +0900

    Jest) roles debugging - logRoles

--- react.js/src/App.test.js
+++ react.js/src/App.test.js
@@ -1,15 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { logRoles, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
-  render(<App />);
+  // Roles Debugging
+  const {container} = render(<App />);
+  logRoles(container);
 
   // find an element with a role of button and text of 'Change to blue'
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
-  
 });
 
 test('button turns blue when clicked', () => {
