e973815b2cf82b222ce54f05b518d971680b22b5
진현주 <97022695+realzu@users.noreply.github.com>
2022-12-14 23:30:24 +0900

    next.ts) install react-image-gallery for carousel

---
+++ next.js-ts/pages/products.tsx
@@ -0,0 +1,22 @@
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+import React from 'react'
+
+export default function Products() {
+    return <ImageGallery items={images} />
+}
