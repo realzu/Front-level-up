8052dc0d6bf7361ab5a84a85da34409055931df0
진현주 <97022695+realzu@users.noreply.github.com>
2022-12-17 23:56:57 +0900

    react) add Todo - Modal - Backdrop component

---
+++ react.js/src/react-components/Modal.js
@@ -0,0 +1,9 @@
+export default function Modal() {
+	return (
+		<div className="modal">
+			<p>Are you sure?</p>
+			<button className="btn btn--alt">Cancel</button>
+			<button className="btn">Cancel</button>
+		</div>
+	);
+}
