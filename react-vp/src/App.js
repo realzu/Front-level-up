db133805d7c2ba22030c38b7746978b6e8db857a
realzu <jinhh501@naver.com>
2023-03-30 11:54:54 +0900

    add: error work

--- react-vp/src/App.js
+++ react-vp/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import MyComponent from './MyComponent';
+// import Counter from './Counter';
+// import Say from './Say';
+import { Component } from 'react';
+import ErrorBoundary from './ErrorBoundary';
+import LiftCycleSample from './LiftCycleSample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777216).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LiftCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
