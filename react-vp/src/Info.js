e60f2195314b2791910b5ce15145088e9af2ef48
realzu <jinhh501@naver.com>
2023-03-30 13:44:08 +0900

    add: reducer - action

---
+++ react-vp/src/Info.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
+
+  const { name, nickname } = state;
+
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <p>name: {name}</p>
+        <p>nickname: {nickname}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
