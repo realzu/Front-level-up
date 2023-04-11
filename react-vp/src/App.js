c6e6d6e5d681a83210ad1788dfc72948d9c2ba4c
realzu <jinhh501@naver.com>
2023-04-11 17:11:28 +0900

    update: separate routers

--- react-vp/src/App.js
+++ react-vp/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import Box from './Box';
+import { Route, Routes } from 'react-router-dom';
+import BasicPage from './BasicComponents/BasicPage';
+import TodoPage from './TodoComponents/TodoPage';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Box ref={(ref) => (this.bbooxx = ref)} />
-        <button onClick={() => this.bbooxx.scrollToBottom()}>맨 밑으로</button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<BasicPage />} />
+      <Route path="/todo" element={<TodoPage />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ react-vp/src/BasicComponents/BasicPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Box from './Box';
+
+export default class BasicPage extends Component {
+  render() {
+    return (
+      <div>
+        <Box ref={(ref) => (this.bbooxx = ref)} />
+        <button onClick={() => this.bbooxx.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
+}
