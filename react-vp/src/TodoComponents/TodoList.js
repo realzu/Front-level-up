e2b4aad2af1edcfcdd0b704f14f1495f1e227ba6
realzu <jinhh501@naver.com>
2023-04-15 15:50:57 +0900

    add: react-virtualized

--- react-vp/src/TodoComponents/TodoList.js
+++ react-vp/src/TodoComponents/TodoList.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
-          key={todo.id}
           todo={todo}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
+    // <div className="TodoList">
+    //   {todos.map((todo) => (
+    //     <TodoListItem
+    //       key={todo.id}
+    //       todo={todo}
+    //       onRemove={onRemove}
+    //       onToggle={onToggle}
+    //     />
+    //   ))}
+    // </div>
   );
 };
 
