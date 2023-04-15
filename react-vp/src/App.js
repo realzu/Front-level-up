d44d8c237dc3b81a7394477c2aa4b80414190906
realzu <jinhh501@naver.com>
2023-04-15 21:40:29 +0900

    add: handle object data

--- react-vp/src/App.js
+++ react-vp/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import BasicPage from './BasicComponents/BasicPage';
 import TodoPage from './TodoComponents/TodoPage';
+import Immutability from './Components/Immutability';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<BasicPage />} />
       <Route path="/todo" element={<TodoPage />} />
+      <Route path="/Immutability" element={<Immutability />} />
     </Routes>
   );
 };
