0a83b9add3c5fc1132efe8b7c28fc494a74085db
realzu <jinhh501@naver.com>
2023-04-16 23:45:35 +0900

    add: query string data

--- react-vp/src/App.js
+++ react-vp/src/App.js
@@ -2,13 +2,28 @@ import { Route, Routes } from 'react-router-dom';
 import BasicPage from './BasicComponents/BasicPage';
 import TodoPage from './TodoComponents/TodoPage';
 import Immutability from './Components/Immutability';
+import Home from './RouteComponents/Home';
+import About from './RouteComponents/About';
+import Profile from './RouteComponents/Profile';
+import Articles from './RouteComponents/Articles';
+import Article from './RouteComponents/Article';
+import Layout from './RouteComponents/Layout';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<BasicPage />} />
+      <Route index element={<BasicPage />} />
       <Route path="/todo" element={<TodoPage />} />
       <Route path="/Immutability" element={<Immutability />} />
+
+      <Route element={<Layout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
     </Routes>
   );
 };
---
+++ react-vp/src/RouteComponents/About.js
@@ -0,0 +1,34 @@
+import { Navigate, useSearchParams } from 'react-router-dom';
+
+const About = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get('detail');
+  const mode = searchParams.get('mode');
+
+  const isLoggedIn = false;
+
+  if (!isLoggedIn) {
+    return <Navigate to="/articles" replace={true} />;
+  }
+
+  const onToggleDetail = () => {
+    setSearchParams({ mode, detail: detail === 'true' ? false : true });
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParams({ mode: nextMode, detail });
+  };
+
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>Toggle detail</button>
+    </div>
+  );
+};
+
+export default About;
