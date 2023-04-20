92fd8b3ec380003cfc7ba6569d45009b8061dd7e
realzu <jinhh501@naver.com>
2023-04-20 21:37:37 +0900

    update: separate pages

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,13 +1,17 @@
-// Jest
-
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
+import BlogPage from "./base-blog-em/BlogPage";
 
 function App() {
-	return (
-		<div>
-			<button style={{backgroundColor: 'red'}} >Change to blue</button>
-		</div>
-	);
+  return (
+    // <div>
+    // 	<button style={{backgroundColor: 'red'}} >Change to blue</button>
+    // </div>
+
+    <Routes>
+      {/* React-query */}
+      <Route path="/" element={<BlogPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- react.js/src/index.js
+++ react.js/src/index.js
@@ -3,33 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import App2 from "./App2";
-import App3 from "./App3";
 import { BrowserRouter } from "react-router-dom";
 import { FavoritesContextProvider } from "./store/favorites-context";
 import App4 from "./App4";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-// 	// <React.StrictMode>
-// 	<>
-// 		<App />
-// 		<App2 />
-// 		<App3 />
-// 	</>
-// 	// </React.StrictMode>
-// );
-
-ReactDOM.render(
-	<FavoritesContextProvider>
-		<BrowserRouter>
-			<App4 />
-		</BrowserRouter>
-	</FavoritesContextProvider>,
-	document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// ReactDOM.render(
+// 	<FavoritesContextProvider>
+// 		<BrowserRouter>
+// 			<App4 />
+// 		</BrowserRouter>
+// 	</FavoritesContextProvider>,
+// 	document.getElementById("root")
+// );
+
 reportWebVitals();
