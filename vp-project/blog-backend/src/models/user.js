ca04da90b2751d4b3750b85d56bac5ef27b2c790
realzu <jinhh501@naver.com>
2023-04-26 18:46:56 +0900

    add: bcrypt

---
+++ vp-project/blog-backend/src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// 인스턴스 메서드
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
