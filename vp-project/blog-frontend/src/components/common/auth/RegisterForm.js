3f68b3d2d901daf816a8aeb695a01a97146f8e59
realzu <jinhh501@naver.com>
2023-05-06 15:16:43 +0900

    add: user redux module

--- vp-project/blog-frontend/src/components/common/auth/RegisterForm.js
+++ vp-project/blog-frontend/src/components/common/auth/RegisterForm.js
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../../modules/auth';
+import { changeField, initializeForm, register } from '../../../modules/auth';
 import AuthForm from './AuthForm';
+import { check } from '../../../modules/user';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
 
   const onChange = (e) => {
@@ -22,12 +28,34 @@ const RegisterForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    //
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      //
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('error');
+      return;
+    }
+    if (auth) {
+      console.log('success');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/'); // home화면
+    }
+  }, [user, navigate]);
 
   return (
     <AuthForm
