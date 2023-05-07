97c8242ee86269b3fef4e9b3c32bc81d21d2ffb9
realzu <jinhh501@naver.com>
2023-05-07 09:35:00 +0900

    update: add login module

--- vp-project/blog-frontend/src/components/common/auth/LoginForm.js
+++ vp-project/blog-frontend/src/components/common/auth/LoginForm.js
@@ -1,12 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from './AuthForm';
-import { changeField, initializeForm } from '../../../modules/auth';
+import { changeField, initializeForm, login } from '../../../modules/auth';
 import { useEffect } from 'react';
+import { check } from '../../../lib/api/auth';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
 
   const onChange = (e) => {
@@ -22,12 +28,29 @@ const LoginForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    //
+    const { username, password } = form;
+    dispatch(login({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('authError');
+    }
+    if (auth) {
+      console.log('success');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
   return (
     <AuthForm
