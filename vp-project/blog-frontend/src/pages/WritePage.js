587fdfd9e5b703fe21eb5cb3d7517f35c484c2c3
realzu <jinhh501@naver.com>
2023-05-07 11:44:09 +0900

    add: add tag-box

--- vp-project/blog-frontend/src/pages/WritePage.js
+++ vp-project/blog-frontend/src/pages/WritePage.js
@@ -1,10 +1,12 @@
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
