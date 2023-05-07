2f082a61d25972f0c02a02f68aa53cee7921d275
realzu <jinhh501@naver.com>
2023-05-07 10:31:20 +0900

    style: add responsive header

---
+++ vp-project/blog-frontend/src/components/common/Header.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import Responsive from './Responsive';
+import Button from './Button';
+import { Link } from 'react-router-dom';
+
+const HeaderBlock = styled.div`
+  position: fixed; // 항상 떠있도록
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+
+const Wrapper = styled(Responsive)`
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .logo {
+    font-size: 1.125rem;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Spacer = styled.div`
+  height: 4rem; // 하단 콘텐츠와 겹침 방지
+`;
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <Link to="/" className="logo">
+            Reacters
+          </Link>
+          <div className="right">
+            <Button to="/login">로그인</Button>
+          </div>
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
+};
+
+export default Header;
